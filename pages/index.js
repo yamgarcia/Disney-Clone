8b0084e9891c9736bf67027e17738a9d3ff10795
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-04-24 22:40:00 -0700

    Structure Sections and banner

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { gql, GraphQLClient } from "graphql-request";
+import Section from "../components/Section";
 
 export const getStaticProps = async () => {
   const url = process.env.ENDPOINT;
@@ -11,10 +12,7 @@ export const getStaticProps = async () => {
 
   const client = new GraphQLClient(url);
 
-  client.setHeader(
-    "authorization",
-    process.env.GRAPH_CMS_TOKEN
-    );
+  client.setHeader("authorization", process.env.GRAPH_CMS_TOKEN);
 
   const query = gql`
     {
@@ -48,7 +46,70 @@ export const getStaticProps = async () => {
 };
 
 const Home = ({ videos }) => {
-  return <>Hello Disney Clone</>;
+  const randomVideo = (videos) => {
+    return videos[Math.floor(Math.random() * videos.length)];
+  };
+
+  const filterVideos = (videos, genre) => {
+    return videos.filter((video) => video.tags.includes(genre));
+  };
+
+  return (
+    <>
+      <div className='app'>
+        <div className='main-video'>
+          <img
+            className='banner'
+            src={randomVideo(videos).thumbnail.url}
+            alt={randomVideo(videos).title}
+          />
+        </div>
+
+        <div className='video-feed'>
+          <Section
+            genre={"Recommended for you"}
+            videos={filterVideos(videos, "Family")}
+          ></Section>
+          <Section
+            genre={"Family"}
+            videos={filterVideos(videos, "Family")}
+          ></Section>
+          <Section
+            genre={"Pixar"}
+            videos={filterVideos(videos, "Pixar")}
+          ></Section>
+          <Section
+            genre={"Disney"}
+            videos={filterVideos(videos, "Disney")}
+          ></Section>
+          <Section
+            genre={"Drama"}
+            videos={filterVideos(videos, "Drama")}
+          ></Section>
+          <Section
+            genre={"Fantasy"}
+            videos={filterVideos(videos, "Fantasy")}
+          ></Section>
+          <Section
+            genre={"Classic"}
+            videos={filterVideos(videos, "Classic")}
+          ></Section>
+          <Section
+            genre={"Thriller"}
+            videos={filterVideos(videos, "Thriller")}
+          ></Section>
+          <Section
+            genre={"Scary"}
+            videos={filterVideos(videos, "Scary")}
+          ></Section>
+          <Section
+            genre={"Horror"}
+            videos={filterVideos(videos, "Horror")}
+          ></Section>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
