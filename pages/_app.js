b9c1bc56b1b1f4b0e1b30c743882fb4f07cf9f99
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-04-26 22:40:59 -0700

    Set up Context API and move states to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import AppContext from "../AppContext";
+import { useState } from "react";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
+  const [watching, setWatching] = useState(false);
+
+  return (
+    <AppContext.Provider
+      value={{
+        state: {
+          watching: watching,
+        },
+        setWatching: setWatching,
+      }}
+    >
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
+};
 
 export default MyApp;
--- pages/video/[slug].js
+++ pages/video/[slug].js
@@ -1,4 +1,8 @@
 import { gql, GraphQLClient } from "graphql-request";
+import Link from "next/Link";
+import { useContext } from "react";
+import AppContext from "../../AppContext";
+import Footer from "../../components/Footer";
 
 export const getServerSideProps = async (pageContext) => {
   const url = process.env.ENDPOINT;
@@ -45,11 +49,43 @@ export const getServerSideProps = async (pageContext) => {
 
 const Video = ({ video }) => {
   console.log(video);
+
+  const value = useContext(AppContext);
+  let { watching } = value.state;
+  let { setWatching } = value;
+
   return (
     <>
-      <div className='main-video'>
-        <img className='banner' src={video.thumbnail.url} alt={video.title} />
-      </div>
+      {/* <div className='main-video'> */}
+      {!watching && (
+        <div className='video-image '>
+          <img className='banner' src={video.thumbnail.url} alt={video.title} />
+        </div>
+      )}
+      {!watching && (
+        <div className='info'>
+          <p>{video.tag}</p>
+          <p>{video.description}</p>
+          <Link href='/'>
+            <p>Back</p>
+          </Link>
+          <button
+            className='video-overlay'
+            onClick={() => {
+              watching ? setWatching(false) : setWatching(true);
+            }}
+          >
+            PLAY
+          </button>
+        </div>
+      )}
+
+      {watching && (
+        <video width='100%' controls autoPlay>
+          <source src={video.mp4.url} type='video/mp4' />
+        </video>
+      )}
+      <Footer />
     </>
   );
 };
